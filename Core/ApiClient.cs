9493bb09a8cadb23c834ab8f95122814daa4ab3d
jasmine-codes <jsmlee97@gmail.com>
2026-01-13 11:58:38 +0100

    create api client class

--- Core/ApiClient.cs
+++ Core/ApiClient.cs
@@ -6,6 +6,14 @@ namespace MiniApiFramework.Core
 {
     public class ApiClient
     {
-        
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(string baseUrl)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new System.Uri(baseUrl);
+            };
+        }
     }
 }
