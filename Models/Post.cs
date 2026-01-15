fd58f278a71d6a68b84e95417787a3258a13fcbb
jasmine-codes <jsmlee97@gmail.com>
2026-01-15 10:28:17 +0100

    create class post

--- Models/Post.cs
+++ Models/Post.cs
@@ -4,6 +4,7 @@ namespace MiniApiFramework.Models
     {
         public int UserId {get; set;}
         public int Id {get; set;}
-        
+        public string? Title {get; set;}
+        public string? Body {get; set;}
     }   
 }
