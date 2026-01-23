175d4eb761069eb120ceb9b6f9f373280ae42ee7
jasmine-codes <jsmlee97@gmail.com>
2026-01-23 14:58:59 +0100

    post - add negative test

--- Tests/PostsTests.cs
+++ Tests/PostsTests.cs
@@ -38,7 +38,7 @@ namespace MiniApiFramework.Tests
         public async Task Get_Invalid_Post_Returns_404()
         {
             var response = await ApiClient.GetAsync("/posts/999999");
-            Assert.That(response.StatusCode, );
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NotFound));
         }
     }
 }
