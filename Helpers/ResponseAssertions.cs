2fc725d34db38650452bbc2cb0767d512c1372e8
jasmine-codes <jsmlee97@gmail.com>
2026-01-28 09:39:36 +0100

    add success assertion

--- Helpers/ResponseAssertions.cs
+++ Helpers/ResponseAssertions.cs
@@ -5,6 +5,10 @@ namespace MiniApiFramework.Helpers
 {
     public static class ResponseAssertions
     {
-        
+        public static void AssertSuccess(HttpResponseMessage response)
+        {
+            Assert.That(response.IsSuccessStatusCode, Is.True,
+            $"Expected success but got {response.StatusCode}");
+        }
     }
 }
