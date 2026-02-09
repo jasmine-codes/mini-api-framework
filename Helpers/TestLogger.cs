75aec514ae1450dfae68126219cc493fb126c799
jasmine-codes <jsmlee97@gmail.com>
2026-02-09 14:43:44 +0100

    create test logger

---
+++ Helpers/TestLogger.cs
@@ -0,0 +1,3 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
--- obj/Debug/net9.0/MiniApiFramework.AssemblyInfo.cs
+++ obj/Debug/net9.0/MiniApiFramework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniApiFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56eed7965334637cb30ec263fbc1c5678db1aaee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81756e62b770425239a85cdb8bc1b9b84595c9d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniApiFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniApiFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
