730198874805ac36f7b77bc4b75dada8bee7784e
jasmine-codes <jsmlee97@gmail.com>
2026-02-09 14:52:26 +0100

    create test logger

--- Helpers/TestLogger.cs
+++ Helpers/TestLogger.cs
@@ -6,9 +6,12 @@ namespace MiniApiFramework.Helpers
 {
     public static class TestLogger
     {
-        public static async Task LogResponse()
+        public static async Task LogResponse(HttpResponseMessage response)
         {
-            
+            Console.WriteLine($"STATUS: {(int)response.StatusCode}");
+            Console.WriteLine("BODY:");
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+            Console.WriteLine("---------------------------");
         }
     }
 }
